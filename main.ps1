fdd1562ade432266e4589ef86be9e02fd1fe9d46
nathsdm <nathan.soares_de_melo@telecom-sudparis.eu>
2023-12-18 11:16:22 +0100

    Changed menu for the main

--- main.ps1
+++ main.ps1
@@ -1,5 +1,44 @@
 # Main entry point for the app
 
+function Show-Menu {
+    param (
+        [string[]]$MenuItems
+    )
+
+    $selectedItem = 0
+
+    while ($true) {
+        Clear-Host
+
+        # Display menu items
+        for ($i = 0; $i -lt $MenuItems.Count; $i++) {
+            if ($i -eq $selectedItem) {
+                Write-Host ("[x] " + $MenuItems[$i]) -ForegroundColor Green
+            } else {
+                Write-Host ("[ ] " + $MenuItems[$i]) -ForegroundColor Gray
+            }
+        }
+
+        # Get user input
+        $key = $Host.UI.RawUI.ReadKey("AllowCtrlC,IncludeKeyUp,NoEcho").VirtualKeyCode
+
+        # Process user input
+        switch ($key) {
+            13 { # Enter key
+                return $MenuItems[$selectedItem]
+            }
+            38 { # Up arrow
+                $selectedItem = ($selectedItem - 1 + $MenuItems.Count) % $MenuItems.Count
+            }
+            40 { # Down arrow
+                $selectedItem = ($selectedItem + 1) % $MenuItems.Count
+            }
+        }
+    }
+}
+
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")
+
 # Define a list of choices
 $choices = @(
     "Manage subtitles",
@@ -8,7 +47,7 @@ $choices = @(
 )
 
 # Display choices in a grid view
-$userChoice = $choices | Out-GridView -Title "Select an Option" -OutputMode Single
+$userChoice = Show-Menu -MenuItems $choices
 
 # Check if the user made a selection
 if ($userChoice) {
